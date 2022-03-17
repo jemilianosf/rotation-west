29a3d27060b94f737b173f3101c69147f806de79
jemilianosf <jemilianosf@gmail.com>
2022-03-17 17:19:37 -0400

    Added run macs script.

--- execution_scripts/run_seacr.sh
+++ execution_scripts/run_seacr.sh
@@ -1,14 +1,18 @@
-# !/bin/bash
+#!/bin/sh
+#SBATCH -J seacr
+#SBATCH --mem=8G
+#SBATCH -a 1-8
 
 
-peaks_dir=$1
+names_file=/data/westlab/jes157/data_input/fastq/chip_zic_dirs.txt
+peaks_dir=$(awk -v i=$SLURM_ARRAY_TASK_ID 'NR==i' $names_file)
 
 
-echo "................Calling peaks with SEACR..."
 cd $peaks_dir
-module load bedtools2
 module load Anaconda3/2019.10-gcb02
 source activate /data/westlab/conda
+
+module load bedtools2
 
 chromSize='/data/westlab/reference/gencode_mouse_vM21/STAR_mm10_2.7.2b/chrNameLength.txt'
 seacr='/data/westlab/genomeTools/SEACR/SEACR_1.3.sh'
