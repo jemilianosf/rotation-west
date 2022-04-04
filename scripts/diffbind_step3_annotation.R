b5f3e42ac2ebfd5ddeb1172e8e531eaf20189878
jemilianosf <jemilianosf@gmail.com>
2022-04-04 14:04:35 -0400

    Made the variable names more general.

--- scripts/diffbind_step3_annotation.R
+++ scripts/diffbind_step3_annotation.R
@@ -3,26 +3,28 @@ library(TxDb.Mmusculus.UCSC.mm10.knownGene)
 txdb <- TxDb.Mmusculus.UCSC.mm10.knownGene
 
 deseq_obj_contrasts <- readRDS("../data_output/diffbind_cutnrun_zic/deseq_obj_contrasts.Rds")
-res_7v1 <- readRDS("../data_output/diffbind_cutnrun_zic/res_7v1_ashr.Rds")
+name_contrast <- "7v3"
+output_dir <- "../data_output/diffbind_cutnrun_zic/"
+res <- readRDS(paste0(output_dir,"res_",name_contrast,"_ashr.Rds"))
 
-peaks_7v1 <- granges(rowRanges(deseq_obj_contrasts[tidyr::replace_na(res_7v1$padj,1) < 0.05 &
-                                             tidyr::replace_na(res_7v1$log2FoldChange,0) > 0]))
-peaks_1v7 <- granges(rowRanges(deseq_obj_contrasts[tidyr::replace_na(res_7v1$padj,1) < 0.05 &
-                                             tidyr::replace_na(res_7v1$log2FoldChange,0) < 0]))
-peaks_all <- granges(rowRanges(deseq_obj_contrasts))
+peaks_up <- GenomicRanges::granges(SummarizedExperiment::rowRanges(deseq_obj_contrasts[tidyr::replace_na(res$padj,1) < 0.05 &
+                                             tidyr::replace_na(res$log2FoldChange,0) > 0]))
+peaks_down <- GenomicRanges::granges(SummarizedExperiment::rowRanges(deseq_obj_contrasts[tidyr::replace_na(res$padj,1) < 0.05 &
+                                             tidyr::replace_na(res$log2FoldChange,0) < 0]))
+peaks_all <- GenomicRanges::granges(SummarizedExperiment::rowRanges(deseq_obj_contrasts))
 
-elementMetadata(peaks_7v1) <- res_7v1[tidyr::replace_na(res_7v1$padj,1) < 0.05 &
-                                        tidyr::replace_na(res_7v1$log2FoldChange,0) > 0]
-elementMetadata(peaks_1v7) <- res_7v1[tidyr::replace_na(res_7v1$padj,1) < 0.05 &
-                                        tidyr::replace_na(res_7v1$log2FoldChange,0) < 0]
+elementMetadata(peaks_up) <- res[tidyr::replace_na(res$padj,1) < 0.05 &
+                                        tidyr::replace_na(res$log2FoldChange,0) > 0,]
+elementMetadata(peaks_down) <- res[tidyr::replace_na(res$padj,1) < 0.05 &
+                                        tidyr::replace_na(res$log2FoldChange,0) < 0,]
 
-elementMetadata(peaks_all) <- res_7v1
+elementMetadata(peaks_all) <- res
 
-peaks_7v1_anno <- annotatePeak(peak = peaks_7v1,TxDb = txdb,
+peaks_up_anno <- annotatePeak(peak = peaks_up,TxDb = txdb,
                                level = "gene",
                                assignGenomicAnnotation = T,
                                annoDb = "org.Mm.eg.db")
-peaks_1v7_anno <- annotatePeak(peak = peaks_1v7,
+peaks_down_anno <- annotatePeak(peak = peaks_down,
                                TxDb = txdb,
                                level = "gene",
                                assignGenomicAnnotation = T,
@@ -33,14 +35,15 @@ peaks_all_anno <- annotatePeak(peak = peaks_all,
                                assignGenomicAnnotation = T,
                                annoDb = "org.Mm.eg.db")
 
-saveRDS(peaks_7v1_anno, file = "../data_output/diffbind_cutnrun_zic/peaks_7v1_up_annotation.Rds")
-saveRDS(peaks_1v7_anno, file = "../data_output/diffbind_cutnrun_zic/peaks_7v1_down_annotation.Rds")
-saveRDS(peaks_all_anno, file = "../data_output/diffbind_cutnrun_zic/peaks_7v1_all_annotation.Rds")
+
+saveRDS(peaks_up_anno, file = paste0(output_dir,"peaks_",name_contrast,"_up_annotation.Rds"))
+saveRDS(peaks_down_anno, file = paste0(output_dir,"peaks_",name_contrast,"_down_annotation.Rds"))
+saveRDS(peaks_all_anno, file = paste0(output_dir,"peaks_",name_contrast,"_all_annotation.Rds"))
 
 
-readr::write_tsv(as.data.frame(peaks_7v1_anno@anno), file = "../data_output/diffbind_cutnrun_zic/peaks_7v1_up_annotation.tsv")
-readr::write_tsv(as.data.frame(peaks_1v7_anno@anno), file = "../data_output/diffbind_cutnrun_zic/peaks_7v1_down_annotation.tsv")
-readr::write_tsv(as.data.frame(peaks_all_anno@anno), file = "../data_output/diffbind_cutnrun_zic/peaks_7v1_all_annotation.tsv")
+readr::write_tsv(as.data.frame(peaks_up_anno@anno), file = paste0(output_dir,"peaks_",name_contrast,"_up_annotation.tsv"))
+readr::write_tsv(as.data.frame(peaks_down_anno@anno), file = paste0(output_dir,"peaks_",name_contrast,"_down_annotation.tsv"))
+readr::write_tsv(as.data.frame(peaks_all_anno@anno), file = paste0(output_dir,"peaks_",name_contrast,"_all_annotation.tsv"))
 
 
 
